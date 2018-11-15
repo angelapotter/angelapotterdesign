5cc7495c19519b0940fea53760c3a02cea400cf2
Angela Potter <angelacbpotter@gmail.com>
2018-11-14 20:07:08 -0800

    Add hover style for gallery item

--- src/components/galleryItem.js
+++ src/components/galleryItem.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 import galleryItemStyles from './galleryItem.module.css'
 
 export default props => (
-  <div key={props.project.slug}>
-    <Img fluid={props.image.node.childImageSharp.fluid} />
-    <div class={galleryItemStyles.name}>
+  <Link to='/project' key={props.project.slug} className={galleryItemStyles.item}>
+    <div className={galleryItemStyles.imageContainer}>
+      <Img className={galleryItemStyles.image} fluid={props.image.node.childImageSharp.fluid} />
+    </div>
+    <div className={galleryItemStyles.name}>
       {props.project.name}
     </div>
-  </div>
+  </Link>
 )
