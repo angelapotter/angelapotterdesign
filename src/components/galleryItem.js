b9e1f64c3463319171ade6ec7be032e1294d8bdb
Angela Potter <angelacbpotter@gmail.com>
2018-11-14 19:48:59 -0800

    Fix spacing

--- src/components/galleryItem.js
+++ src/components/galleryItem.js
@@ -1,9 +1,13 @@
 import React from "react"
 import Img from "gatsby-image"
 
+import galleryItemStyles from './galleryItem.module.css'
+
 export default props => (
   <div key={props.project.slug}>
     <Img fluid={props.image.node.childImageSharp.fluid} />
-    {props.project.name}
+    <div class={galleryItemStyles.name}>
+      {props.project.name}
+    </div>
   </div>
 )
